9368ff4673081e48266e4e9320c76e7ef6396cda
spousty <spousty@redhat.com>
2012-08-29 20:31:26 -0700

    duh add mongo

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var Db = require('mongodb').Db,
   Connection = require('mongodb').Connection,
   ObjectID = require('mongodb').ObjectID;
 
-  var db = new Db('todolist', new Server(process.env.OPENSHIFT_NOSQL_DB_HOST, process.env.OPENSHIFT_NOSQL_DB_PORT, {auto_reconnect: true
+  var db = new Db('todolist', new Server(process.env.OPENSHIFT_NOSQL_DB_HOST, parseInt(process.env.OPENSHIFT_NOSQL_DB_PORT), {auto_reconnect: true
 }));
 
 
